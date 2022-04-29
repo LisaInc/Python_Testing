c4657a95a5420e93e3cd5ba38cb60aa0a4db1452
LisaInc <incardonalisa@gmail.com>
2022-04-29 15:47:11 +0200

    Fix issue 2 and add test

--- server.py
+++ server.py
@@ -53,11 +53,19 @@ def create_app():
         ][0]
         club = [c for c in clubs if c["name"] == request.form["club"]][0]
         placesRequired = int(request.form["places"])
-        competition["numberOfPlaces"] = (
-            int(competition["numberOfPlaces"]) - placesRequired
-        )
-        flash("Great-booking complete!")
-        return render_template("welcome.html", club=club, competitions=competitions)
+        if placesRequired <= int(club["points"]):
+            competition["numberOfPlaces"] = (
+                int(competition["numberOfPlaces"]) - placesRequired
+            )
+            flash("Great-booking complete!")
+            return render_template("welcome.html", club=club, competitions=competitions)
+        else:
+            return render_template(
+                "booking.html",
+                club=club,
+                competition=competition,
+                message="Your club don't have enought points",
+            )
 
     # TODO: Add route for points display
 
--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -1,8 +1,7 @@
+import pytest
 from tests.conftest import client
-from server import loadClubs, loadCompetitions
-
-clubs = loadClubs()
-competitions = loadCompetitions()
+from server import loadClubs
+import server
 
 
 def test_index(client):
@@ -10,19 +9,71 @@ def test_index(client):
     assert response.status_code == 200
 
 
-def test_showSummary(client):
+def test_showSummary_valid_email(client):
+    clubs = server.loadClubs()
+    competitions = server.loadCompetitions()
+    print(clubs)
     rv = client.post(
-        "/showSummary", data=dict(email="john@simplylift.co"), follow_redirects=True
+        "/showSummary", data=dict(email=clubs[0]["email"]), follow_redirects=True
     )
-    assert rv.status_code == 200
     data = rv.data.decode()
+    print(data)
+    assert rv.status_code == 200
     assert clubs[0]["email"] in data
     assert f"Points available: {clubs[0]['points']}"
     for competition in competitions:
         assert competition["name"] in data
+
+
+def test_showSummary_invalid_email(client):
     rv = client.post(
         "/showSummary", data=dict(email="wrong@gmail.com"), follow_redirects=True
     )
-    assert rv.status_code == 200
     data = rv.data.decode()
+
+    assert rv.status_code == 200
     assert "Email not found, try again" in data
+
+
+# def test_book(client):
+#     # rv = client.get("/book", )
+#     pass
+
+
+def test_purchasePlaces_valid_points(client):
+    clubs = server.loadClubs()
+    competitions = server.loadCompetitions()
+    club_points_before = int(clubs[0]["points"])
+    competitions_places_before = int(competitions[0]["numberOfPlaces"])
+    rv = client.post(
+        "/purchasePlaces",
+        data=dict(
+            club=clubs[0]["name"], competition=competitions[0]["name"], places=10
+        ),
+        follow_redirects=True,
+    )
+    data = rv.data.decode()
+    assert rv.status_code == 200
+    # assert clubs[0]["points"] == club_points_before - 10
+    # assert competitions[0]["numberOfPlaces"] == competitions_places_before - 10
+    assert "Great-booking complete!" in data
+
+
+def test_purchasePlaces_invalid_points(client):
+    clubs = server.loadClubs()
+    competitions = server.loadCompetitions()
+    club_points_before = clubs[0]["points"]
+    competitions_places_before = competitions[0]["numberOfPlaces"]
+    rv = client.post(
+        "/purchasePlaces",
+        data=dict(
+            club=clubs[0]["name"], competition=competitions[0]["name"], places=20
+        ),
+        follow_redirects=True,
+    )
+    data = rv.data.decode()
+
+    assert rv.status_code == 200
+    assert "Your club don&#39;t have enought points" in data
+    assert clubs[0]["points"] == club_points_before
+    assert competitions[0]["numberOfPlaces"] == competitions_places_before
